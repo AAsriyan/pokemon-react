f41f552bdabf468897c887ed4088d8eda9a46969
Arshak Asriyan <45574365+AAsriyan@users.noreply.github.com>
2019-02-18 14:22:22 -0500

    Initial commit

---
+++ src/Pokemon.js
@@ -0,0 +1,67 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+
+function Pokemon(props) {
+  return (
+    <>
+      {props.pokemon.map(pokemon => (
+        <div key={pokemon.id} className="pokemon">
+          <img src={pokemon.img} alt={pokemon.name} />
+          <div>
+            <h3>{pokemon.name}</h3>
+
+            {pokemon.next_evolution
+              ? pokemon.next_evolution.map((ev, index) => (
+                  <Fragment key={index}>
+                    Next Evolution:
+                    <p>{ev.name}</p>
+                  </Fragment>
+                ))
+              : null}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+Pokemon.propTypes = {
+  pokemon: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      img: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      type: PropTypes.arrayOf(
+        PropTypes.oneOf([
+          "Grass",
+          "Poison",
+          "Fire",
+          "Flying",
+          "Water",
+          "Bug",
+          "Normal",
+          "Electric",
+          "Psychic",
+          "Ground",
+          "Fighting",
+          "Rock",
+          "Ice",
+          "Ghost",
+          "Dragon"
+        ])
+      ),
+      next_evolution: PropTypes.arrayOf(
+        PropTypes.shape({
+          num: PropTypes.string,
+          name: PropTypes.string
+        })
+      )
+    })
+  )
+};
+
+Pokemon.defaultProps = {
+  pokemon: []
+};
+
+export default Pokemon;
